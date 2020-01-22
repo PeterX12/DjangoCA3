c93a01e5ffbf0c3bc327af011a6f41339c34722a
Peter Lucan <peteroluc@gmail.com>
2020-01-22 14:01:23 +0000

    Version 3.2

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
-from django.contrib import admin
 
 from boards import views
 
 urlpatterns = [
     path(r'admin/', admin.site.urls),
-    url(r'^$', views.home, name='home')
+    url(r'^$', views.home, name='home'),
+    url(r'^boards/(?<pk>\d+)/$', views.board_topics, name='board_topics'),
 ]
