718056d1b19333cd2acd45a0f9fe0c900dd88137
Peter Lucan <peteroluc@gmail.com>
2020-01-29 11:46:39 +0000

    Restarting from 3.0

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
 from django.urls import path , re_path
+from django.contrib import admin
+
 from boards import views
 
 urlpatterns = [
     re_path(r'^$', views.home, name='home'),
-    re_path('boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     re_path(r'^admin/', admin.site.urls),
 ]
